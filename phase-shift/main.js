30f166f30b93ba44ff160e4af469d1bcfcf406b5
ChrisWhealy <chris@whealy.com>
2024-12-06 08:41:23 +0000

    Refactor JS file names

---
+++ phase-shift/main.js
@@ -0,0 +1,25 @@
+// Define phase shift graph
+let twoWavesBox = JXG.JSXGraph.initBoard('twoWavesBox', {
+  boundingbox: [-15, 1.25, 375, -1.25],
+  axis: true,
+  defaultAxes: { x: { ticks: angleTicks } },
+  showCopyright: false,
+  showNavigation: false,
+})
+twoWavesBox.containerObj.style.backgroundColor = OFF_WHITE_BLUE
+
+let sliderText = twoWavesBox.create('text', [85, 1.1, 'Phase shift']);
+let cosineSlider = twoWavesBox.create('slider', [[120, 1.1], [250, 1.1], [0, 90, 360]], { snapWidth: 1, precision: 0 });
+
+// Draw the sine and cosine waves
+let sine = twoWavesBox.create(
+  'functiongraph',
+  [x => Math.sin(x / DEGREES_PER_RADIAN)],
+  { size: 1, name: 'A', strokeColor: 'green' }
+)
+let cosine = twoWavesBox.create(
+  'functiongraph',
+  [x => Math.cos((x + cosineSlider.Value() - 90) % 360 / DEGREES_PER_RADIAN)],
+  { size: 1, name: 'B', strokeColor: 'blue' }
+)
+
