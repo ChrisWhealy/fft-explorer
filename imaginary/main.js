4db8707aa2fb4c9bc3e63969770b02f078c6b2e1
ChrisWhealy <chris@whealy.com>
2024-12-10 11:16:19 +0000

    Update imaginary page

--- imaginary/main.js
+++ imaginary/main.js
@@ -12,7 +12,7 @@ let imagTicks = {
   label: 'Imaginary',
 }
 
-// Define phase shift graph
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 let imagRotBox = JXG.JSXGraph.initBoard('imagRotBox', {
   boundingbox: [-1.25, 1.25, 1.25, -1.25],
   axis: true,
@@ -30,39 +30,25 @@ let up = imagRotBox.create('point', [0, 1], { visible: false })
 let left = imagRotBox.create('point', [-1, 0], { visible: false })
 let down = imagRotBox.create('point', [0, -1], { visible: false })
 
-let rotation = 0
-let value = 1
 let p = imagRotBox.create(
   'point',
   [1, 0],
   { name: '', face: 'o', size: 5, strokeColor: 'red', fillOpacity: 0.3, strokeOpacity: 0.3 }
 )
-let arrow = imagRotBox.create('arrow', [[0,0], p])
-imagRotBox.create('text', [0.6, 0.75, () => `i<sup>${rotation}</sup> = ${value}`], { fontSize: 18 });
+let arrow = imagRotBox.create('arrow', [[0, 0], p])
 
-setInterval(() => {
-  rotation = (rotation + 1) % 4
+let steps = [
+  { point: right, value: "1" },
+  { point: up, value: "i" },
+  { point: left, value: "-1" },
+  { point: down, value: "-i" }
+]
+let rotation = 0
 
-  let newP
-
-  switch (rotation) {
-    case 1:
-      newP = up
-      value = 'i'
-      break
-    case 2:
-      newP = left
-      value = '-1'
-      break
-    case 3:
-      newP = down
-      value = '-i'
-      break
-    default:
-      newP = right
-      value = '1'
+imagRotBox.create('text', [0.6, 0.75, () => `i<sup>${rotation}</sup> = ${steps[rotation].value}`], { fontSize: 18 });
+setInterval(
+  () => {
+    rotation = (rotation + 1) % 4
+    p.moveTo([steps[rotation].point.X(), steps[rotation].point.Y()], 1600)
   }
-
-  p.moveTo([newP.X(), newP.Y()], 1600)
-}
   , 2000)
