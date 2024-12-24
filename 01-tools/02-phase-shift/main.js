5d73f63b58f861a568ba5fac7c0a8cdc2bbde211
ChrisWhealy <chris@whealy.com>
2024-12-24 14:08:28 +0000

    Improve description of phase to rotation

--- 01-tools/02-phase-shift/main.js
+++ 01-tools/02-phase-shift/main.js
@@ -1,7 +1,32 @@
+const LOWER_BOUND = 0
+const UPPER_BOUND = 360
+const CIRCLE_BOUND = 1.25
+
+let idx = 0
+
 // ---------------------------------------------------------------------------------------------------------------------
-// Define phase shift graph
+// Define circle and phase shift graphs
+let circleBox = JXG.JSXGraph.initBoard('circleBox', {
+  boundingbox: [-CIRCLE_BOUND, CIRCLE_BOUND, CIRCLE_BOUND, -CIRCLE_BOUND],
+  keepaspectratio: true,
+  axis: true,
+  showCopyright: false,
+  showNavigation: false,
+})
+circleBox.containerObj.style.backgroundColor = OFF_WHITE_BLUE
+
+let origin = circleBox.create('point', [0, 0], { name: '', size: 1, face: 'o', fixed: true })
+let edge = circleBox.create('point', [1, 0], { name: '', visible: false, fixed: true })
+let circle = circleBox.create('circle', [origin, edge], circumferenceStyle)
+let circumference = circleBox.create('glider', [0.8, 0.6, circle], { name: 'C' })
+let radius = circleBox.create('line', [origin, circumference], radiusLineStyle)
+
+let rightPoint = circleBox.create('point', [1.25, () => circumference.Y()], { size: 1, visible: false })
+let horizLine = circleBox.create('line', [circumference, rightPoint], edgeLineStyle)
+
+// ---------------------------------------------------------------------------------------------------------------------
 let twoWavesBox = JXG.JSXGraph.initBoard('twoWavesBox', {
-  boundingbox: [-15, 1.25, 375, -1.25],
+  boundingbox: [-15, 1.25, UPPER_BOUND + 15, -1.25],
   axis: true,
   defaultAxes: { x: { ticks: angleTicks } },
   showCopyright: false,
@@ -9,14 +34,16 @@ let twoWavesBox = JXG.JSXGraph.initBoard('twoWavesBox', {
 })
 twoWavesBox.containerObj.style.backgroundColor = OFF_WHITE_BLUE
 
-let sliderText = twoWavesBox.create('text', [85, 1.1, 'Phase shift']);
-let cosineSlider = twoWavesBox.create('slider', [[120, 1.1], [250, 1.1], [0, 90, 360]], { snapWidth: 1, precision: 0 });
-
 // ---------------------------------------------------------------------------------------------------------------------
 // Draw the sine and cosine waves
-let sine = twoWavesBox.create('functiongraph', [x => sineDegrees(x)], { size: 1, name: 'A', strokeColor: 'green' })
-let cosine = twoWavesBox.create(
-  'functiongraph',
-  [x => sineDegrees(x + cosineSlider.Value())],
-  { size: 1, name: 'B', strokeColor: 'blue' }
-)
+let sin1 = twoWavesBox.create('functiongraph', [x => sineDegrees(x), LOWER_BOUND, UPPER_BOUND], { size: 1, strokeColor: 'green' })
+let sin2 = twoWavesBox.create('functiongraph', [x => cosineDegrees(x), LOWER_BOUND, UPPER_BOUND], { size: 1, strokeColor: 'blue' })
+
+const updatePhase = () => {
+  const deg = Math.atan2(circumference.Y(), circumference.X()) * DEGREES_PER_RADIAN
+  sin2.Y = x => sineDegrees(x + deg)
+  twoWavesBox.update()
+}
+
+circumference.on('drag', updatePhase)
+updatePhase()
